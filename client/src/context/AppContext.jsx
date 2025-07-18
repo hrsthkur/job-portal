0bb4571c57459cf466836b33f6858d32c033bcce
Harsh Singh <hrsthkur1@gmail.com>
2025-07-18 22:46:17 +0530

    started building job portal application

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+// eslint-disable-next-line react-refresh/only-export-components
+export const AppContext = createContext()
+export const AppContextProvider = (props) => {
+    const value = {
+
+    }
+
+    return (<AppContext.Provider value={value}>
+        {props.children}
+    </AppContext.Provider>
+
+    )
+}
