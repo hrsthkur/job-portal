d006c53a45967dea501f4572bf3200ab625932ce
Harsh Singh <hrsthkur1@gmail.com>
2025-07-24 08:27:28 +0530

    added authentication feature

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-import { UserButton, useUser,useClerk } from '@clerk/clerk-react'
-
+import { UserButton, useUser ,useClerk } from '@clerk/clerk-react'
+    import { Link } from 'react-router-dom';
+import Applications from '../pages/Applications';
 const Navbar = () => { 
 
   const {openSignIn} = useClerk()
+  const {user} = useUser()
   return (
     <div className='shadow py-4'>
         <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center'>
         <img src={assets.logo} alt="" />
-        <div className='flex gap-4 max-sm:text-xs'>
+        {
+          user
+          ?<div>
+            <Link to={Applications}>Applied jobs</Link>
+            <p>|</p>
+            <p>Hi, {user.firstName + " " + user.lastName} </p>
+            <UserButton></UserButton>
+          </div>
+          : <div className='flex gap-4 max-sm:text-xs'>
             <button className='text-gray-600'>Recruiter Login</button>
             <button onClick={ e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
         </div>
+       
+        }
         </div>
       
     </div>
