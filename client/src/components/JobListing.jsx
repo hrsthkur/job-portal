0571117297df7b05e5d7a9f78708667ceecb8c28
Harsh Singh <hrsthkur1@gmail.com>
2025-07-29 09:47:27 +0530

    added filter functionality

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,42 +1,82 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
+import { assets, JobCategories, JobLocations } from '../assets/assets'
 
 const JobListing = () => {
 
-    const {isSearched, searchFilter} = useContext(AppContext)
+    const {isSearched, searchFilter, setSearchFilter} = useContext(AppContext)
 
 
   return (
-    <div>
+    <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
 
         {/* sidebar */}
-        <div>
+        <div className='w-full lg:w-1/4 bg-white px-4'>
             {/* Search filter from Hero component */}
             {
 
             isSearched && (searchFilter.title !== "" || searchFilter.location !== "") && 
             (<>
-            <h3>Current Search</h3>
-            <div>
+            <h3 className='font-medium text-lg mb-4'>Current Search</h3>
+            <div className='mb-4 text-gray-600'>
                 { searchFilter.title && (
-                    <span>
+                    <span className='inline-flex items-center gap-2.5 bg-blue-50 border border-blue-200 px-4 py-1.5 rounded'>
                         {searchFilter.title}
+                        <img onClick={() => setSearchFilter(prev => ({...prev,title: ""}))} src={assets.cross_icon} alt="" className='cursor-pointer'/>
 
                     </span>
                 )}
 
                 {searchFilter.location && (
-                    <span>
+                    <span className='ml-2 inline-flex items-center gap-2.5 bg-red-50 border border-red-200 px-4 py-1.5 rounded'>
                         {searchFilter.location}
+                         <img onClick={() => setSearchFilter(prev => ({...prev,location: ""}))} src={assets.cross_icon} alt="" className='cursor-pointer'/>
                         
                     </span>
+                   
                 )}
             </div>
 
             </>
             )
         }
+
+        {/* search by filter */}
+        <div className='max-lg:hidden'>
+            <h4 className='font-medium text-lg py-4'>Search by filter</h4>
+            <ul className='space-y-4 text-gray-600'>
+                {
+                    JobCategories.map((category,index)=> (
+                        <li className='flex gap-3 items-center' key={index}>
+                            <input className='scale-125' type="checkbox" name="" id="" />
+                            {category}
+
+
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
+
+         {/* search by location */}
+        <div className='max-lg:hidden'>
+            <h4 className='font-medium text-lg py-4'>Search by location</h4>
+            <ul className='space-y-4 text-gray-600'>
+                {
+                    JobLocations.map((location,index)=> (
+                        <li className='flex gap-3 items-center' key={index}>
+                            <input className='scale-125' type="checkbox" name="" id="" />
+                            {location}
+
+
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+        </div>
+
+        {/* Job Listings */}
       
     </div>
   )
