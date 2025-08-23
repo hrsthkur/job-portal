ac91a3b41d85883f43f2b7efe9ef117cedb726ad
Harsh Singh <hrsthkur1@gmail.com>
2025-08-24 00:28:06 +0530

    completed frontend for user

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -6,6 +6,8 @@ import Navbar from '../components/Navbar'
 import { assets } from '../assets/assets'
 import kconvert from 'k-convert';
 import moment from 'moment';
+import JobCards from '../components/JobCards'
+import Footer from '../components/Footer'
 
 
 const ApplyJob = () => {
@@ -65,8 +67,30 @@ const ApplyJob = () => {
             <p className='mt-1 text-gray-600'>Posted {moment(jobData.date).fromNow()}</p>
           </div>
         </div>
+
+        <div className='flex flex-col lg:flex-row justify-between items-start'>
+          <div className=' w-full lg:w-2/3'>
+            <h2 className='font-bold text-2xl mb-4'>Job description</h2>
+            <div className='rich-text' dangerouslySetInnerHTML={{__html:jobData.description}}>
+             
+            </div>
+            <button className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>Apply now</button>
+            </div>
+            {/* Right section more jobs */}
+            <div className='w-full lg:w-1/3 mt-8 lg:mt-0 lg:ml-9 space-y-5'>
+              <h2>More jobs from {jobData.companyId.name}</h2>
+              {jobs.filter( job => job._id !== jobData._id && job.companyId._id === jobData.companyId._id)
+              .filter( job => true).slice(0,4)
+              .map((job,index) => <JobCards key={index} job={job}/>)}
+            </div>
+          
+
+
+
+        </div>
       </div>
     </div>
+    <Footer/>
     </>
   ) : (
     <Loading/>
